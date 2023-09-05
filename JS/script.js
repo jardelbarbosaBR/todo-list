d5e82dd17aa1fef7e6f5380a145af04fe84eaa36
Jardel Barbosa <jardelsantos1111@gmail.com>
2023-09-05 18:34:56 -0300

    Nova Funcionalidades
    Foi adicionado nova funcionalidade: Quando o usuário clicar no botão "Adicionar", o texto que o usuário escreveu vai aparecer como uma lista em baixo da caixa de texto

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+const inputTarefa = document.querySelector('#inputTarefa')
+const listaDeTarefas = document.querySelector('#listartarefas')
+
+let minhaListaDeTarefa = [];
+
+function addTarefa(){
+    minhaListaDeTarefa.push(inputTarefa.value)
+    exibirTarefas()
+}
+
+function exibirTarefas() {
+    let novaLi = ''
+    minhaListaDeTarefa.forEach((intem) =>{
+        novaLi = novaLi + `
+                <li class="list-group-item border-0 d-flex align-items-center ps-0">
+                    <input class="form-check-input me-3" type="checkbox" value="" aria-label="..." />
+                    ${intem}
+                </li>
+            `
+    })
+    listaDeTarefas.innerHTML = novaLi
+}
